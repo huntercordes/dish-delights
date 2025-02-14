8cc57a0cb1f342bf18b23fa9af1fa2cdeb483da7
huntercordes <huntercordes@gmail.com>
2025-02-14 05:41:25 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import Home from "./pages/Home";
 import Recipes from "./pages/Recipes";
